0e3da703f3a6b8cfb76ae4ee3ac0f7c3175ea160
Graeme Hiebert <graeme.hiebert@safe.com>
2019-12-22 07:36:45 -0800

    Initial commit

---
+++ TallyUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TallyUp
+//
+//  Created by Graeme Hiebert on 2019-12-14.
+//  Copyright © 2019 Graeme Hiebert. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
